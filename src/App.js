0cfb162d8e26b09698781e1bc194ddad9366274b
zhangtao615 <295564283@qq.com>
2020-08-15 22:24:22 +0800

    left-panel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import FileSearch from './components/FileSearch'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container-fluid">
+      <div className="row">
+        <div className="col-3 bg-light left-panel">
+          <FileSearch 
+            title="我的云文档"
+            onFileSearch={()=>{console.log('hello')}}
+          ></FileSearch>
+        </div>
+        <div className="col-9 bg-primary right-panel">
+          right
+        </div>
+      </div>
     </div>
   );
 }
