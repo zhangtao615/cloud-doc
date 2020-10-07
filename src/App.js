4576847f591bb550df0163bb2c37ec54658b4500
zhangtao615 <295564283@qq.com>
2020-10-07 15:51:25 +0800

    给组件添加状态

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Fragment, useState} from 'react';
 import FileSearch from './components/FileSearch'
 import FileList from './components/FileList'
 import defaultFiles  from './utils/defaultFiles'
@@ -11,21 +11,29 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  const [files, setfiles] = useState(defaultFiles)
+  const [activeFileID, setActiveFileID] = useState('')
+  const [openFileIDs, setOpenFileIDs] = useState([])
+  const [unsavedFileIDs, setUnsavedFileIDs] = useState([])
+  const openedFiles = openFileIDs.map(openID => {
+    return files.find(files => files.id === openID)
+  })
+  const activeFile = files.find(files => files.id === activeFileID)
   return (
     <div className="App container-fluid px-0">
       <div className="row  no-gutters">
-        <div className="col-3 bg-light left-panel">
+        <div className="col-3 bg-light left-pannel">
           <FileSearch 
             title="我的云文档"
             onFileSearch={(value)=>{console.log(value)}}
           ></FileSearch>
           <FileList
-           files={defaultFiles}
+           files={files}
            onFileClick={(id)=>{}}
            onFileDelete={(id)=>{}}
            onSaveEdit={(id,newVal)=>{alert(newVal)}}
           ></FileList>
-          <div className="row  no-gutters">
+          <div className="row no-gutters button-group">
             <div className="col">
               <Button
                 text="新建"
@@ -43,24 +51,33 @@ function App() {
           </div>
         </div>
         <div className="col-9 right-panel">
-          <TabList
-            files={defaultFiles}
-            unsavedIds={["1"]}
-            onTabClick={(id) => {console.log(id)}}
-            onCloseTab={(id) => {console.log(id)}}
-            activedId="1"
-          ></TabList>
-          <SimpleMDE className="mt-2"
-            value={defaultFiles[1].body}
-            options={{
-              minHeight:'620px',
-              autosave: {
-                enabled:true,
-                delay:1000
-              },
-              tabSize: 2
-            }}
-          ></SimpleMDE>
+          {!activeFile && 
+            <div className="start-page">
+              选择或者创建新的 Markdown 文档
+            </div>
+          }
+          { activeFile && 
+            <Fragment>
+              <TabList
+                files={openedFiles}
+                unsavedIds={unsavedFileIDs}
+                onTabClick={(id) => {console.log(id)}}
+                onCloseTab={(id) => {console.log(id)}}
+                activedId={activeFileID}
+              ></TabList>
+              <SimpleMDE className="mt-2"
+                value={activeFile && activeFile.body}
+                options={{
+                  minHeight:'620px',
+                  autosave: {
+                    enabled:true,
+                    delay:1000
+                  },
+                  tabSize: 2
+                }}
+              ></SimpleMDE>
+          </Fragment>
+        }
         </div>
       </div>
     </div>
